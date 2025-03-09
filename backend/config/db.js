baf4ef7ed26eb733e246cbb51d5025255cc7a843
Shreejan <shreejanid123@gmail.com>
2025-03-09 20:41:07 +0545

    Add MongoDB connection URI to environment configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
+import "dotenv/config";
 
 export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://parajulikiran:185120@cluster0.r0252.mongodb.net/restro"
-    )
-    .then(() => console.log("MongoDB connected"))
-    .catch((err) => console.log(err));
+  try {
+    const conn = await mongoose.connect(
+      process.env.MONGODB_URI ||
+        "mongodb+srv://parajulikiran:185120@cluster0.r0252.mongodb.net/restro"
+    );
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
